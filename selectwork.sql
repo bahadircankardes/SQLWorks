854fb1711b9ab501b795e649c916840f8e347fec
bahadircankardes <kardes126@gmail.com>
2024-08-21 21:58:04 +0300

    selectwork

---
+++ selectwork.sql
@@ -0,0 +1,12 @@
+/*
+select * from customers
+select * from shippers
+select * from employees
+select * from categories
+*/
+
+select customer_id, company_name, contact_name from Customers
+
+select Company_name as Sirket_Adi, Address adres, city sehir from Customers
+
+select * from Customers where Country = 'Germany'
