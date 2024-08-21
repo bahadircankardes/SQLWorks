cb83369c84c0362b44eaf1c4f6eb6d1e2156e053
bahadircankardes <kardes126@gmail.com>
2024-08-21 22:26:17 +0300

    update

--- selectwork.sql
+++ selectwork.sql
@@ -10,3 +10,18 @@ select customer_id, company_name, contact_name from Customers
 select Company_name as Sirket_Adi, Address adres, city sehir from Customers
 
 select * from Customers where Country = 'Germany'
+--Berlin sehrindeki müsterilerin listesi
+select *
+from customers
+where Country = 'Germany' and city = 'Berlin'
+--urunler tablosuna göz atalım
+select * from products;
+select * from suppliers;
+select * from Categories;
+
+--1 numaralı kategoryie ait urunlerin listesi
+select * from products where category_id = 1;
+
+-- siparisler tablosuna goz atalım
+
+select * from orders
