1e8f976fcdf8830e8df21a905c6124d2751fbe6a
bahadircankardes <kardes126@gmail.com>
2024-08-27 14:44:55 +0300

    caseWhen

--- caseWhen.sql
+++ caseWhen.sql
@@ -4,4 +4,14 @@ case (country)
     when 'UK' then 'Ingiltere'
 end Ulke
 from employees
+--************************
+
+select product_name, unit_price,
+case
+    when unit_price < 50 then 'Ucuz Urun'
+    when unit_price > 50 and unit_price < 120 then 'Ekonomik Urun'
+    when unit_price > 120 and unit_price < 200 then 'Pahali Urun'
+    else 'Luks Urun'
+end uruntanimi
+from products order by unit_price desc;
 
