db8f573721be10b1895894eccb0de762ed2a2f74
bahadircankardes <kardes126@gmail.com>
2024-09-16 14:56:05 +0300

    ConsoleDb

--- ConsoleDb/Program.cs
+++ ConsoleDb/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace ConsoleDb;
+﻿using Npgsql;
+
+namespace ConsoleDb;
+
 
 class Program
 {
+    static string constr = "Server =127.0.0.1; Port =5432; Database =Northwind; User Id=postgres; Password=41264126";
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string sql = "select * from shippers";
+        NpgsqlConnection conn = new NpgsqlConnection(constr);
+        NpgsqlCommand cmd = conn.CreateCommand();
+        cmd.CommandText = sql;
+
+        try
+        {
+            conn.Open();
+            NpgsqlDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + " " + rdr[1] + " " + rdr[2]);
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        finally
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+                conn.Close();
+        }
+
+
+
     }
 }
 
