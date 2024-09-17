ea9e32adf12b8e38d672c8ac7433581aa7da05c0
bahadircankardes <kardes126@gmail.com>
2024-09-17 11:17:59 +0300

    SQlite ile C#

--- ConsoleSqlite/Program.cs
+++ ConsoleSqlite/Program.cs
@@ -1,14 +1,24 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using Microsoft.Data.Sqlite;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ConsoleSqlite;
 
 class Program
 {
-    static string constr = "Data Source=/Users/bahadircankardes/source/repos/Wissen2024/deneme.db;Version=3";
 
     static void Main(string[] args)
     {
         
+        string constr = "Data Source=/Users/bahadircankardes/source/repos/Wissen2024/deneme.db";
+        SqliteConnection conn = new SqliteConnection(constr);
+        conn.Open();
+        string sql = "insert into Categories (CategoryName,Description,CreateDate)";
+        sql = sql + "values('Gida', 'Yeme Icme', '01.01.2024')";
+        SqliteCommand cmd = new SqliteCommand(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        
+
 
     }
 }
