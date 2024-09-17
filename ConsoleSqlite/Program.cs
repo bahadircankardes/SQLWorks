9f3a91966b7c324ac5838edd615c922e9d7966ce
bahadircankardes <kardes126@gmail.com>
2024-09-17 10:45:07 +0300

    qwe

--- ConsoleSqlite/Program.cs
+++ ConsoleSqlite/Program.cs
@@ -1,13 +1,14 @@
-﻿namespace ConsoleSqlite;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleSqlite;
 
 class Program
 {
+    static string constr = "Data Source=/Users/bahadircankardes/source/repos/Wissen2024/deneme.db;Version=3";
+
     static void Main(string[] args)
     {
-
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
-
+        
 
     }
 }
